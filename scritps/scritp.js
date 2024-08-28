1fc736c4bbdd55537211308e1b7c084af874fa61
Moncadf <49532150+Moncadf@users.noreply.github.com>
2024-08-27 21:58:08 -0500

    Update scritp.js

--- scritps/scritp.js
+++ scritps/scritp.js
@@ -1,11 +1,11 @@
-// Selección de elementos del DOM
+
 const inputTextArea = document.getElementById('inputTextArea');
 const textoSalida = document.getElementById('textoSalida');
 const btnEncrypt = document.getElementById('btnEncrypt');
 const btnDecrypt = document.querySelector('.btn-decrypt');
 const btnCopy = document.getElementById('btnCopiar');
 
-// Función para encriptar el texto
+
 function encrypt(text) {
     let encryptedText = text
         .replace(/e/g, 'enter')
@@ -16,7 +16,7 @@ function encrypt(text) {
     return encryptedText;
 }
 
-// Función para desencriptar el texto
+
 function decrypt(text) {
     let decryptedText = text
         .replace(/enter/g, 'e')
@@ -27,7 +27,7 @@ function decrypt(text) {
     return decryptedText;
 }
 
-// Evento para encriptar el texto
+
 btnEncrypt.addEventListener('click', () => {
     const inputText = document.getElementById("inputTextArea");
     const encryptedText = encrypt(inputText.value);
@@ -35,7 +35,7 @@ btnEncrypt.addEventListener('click', () => {
     mostrarTextoEnOutput();
 });
 
-// Evento para desencriptar el texto
+
 btnDecrypt.addEventListener('click', () => {
     const inputText = inputTextArea.value;
     const decryptedText = decrypt(inputText);
@@ -43,7 +43,7 @@ btnDecrypt.addEventListener('click', () => {
     mostrarTextoEnOutput();
 });
 
-// Función para mostrar el texto en la salida y ocultar el mensaje de "Ningún mensaje fue encontrado"
+
 function mostrarTextoEnOutput() {
     document.querySelector('.contenedor-salida-vacio').style.display = 'none';
     document.querySelector('.contenedor-salida-lleno').style.display = 'block';
